98a47b771836d647ac76da184e5a632a6a7fa407
Ben O'Steen <bosteen@gmail.com>
2010-08-02 11:30:46 +0100

    [l] Altered the local pairtree implementation to match API and changed testsuite to reflect that.

--- test/test.py
+++ test/test.py
@@ -16,44 +16,52 @@ class TestPairtreeOFS(unittest.TestCase):
     def test_empty(self):
         pass
     
-    def test_claimid(self):
-        a = self.o.claim_an_id()
+    def test_claim_bucket(self):
+        a = self.o.claim_a_bucket()
         self.assertTrue(self.o.exists(a))
     
     def test_store_bytes_no_params(self):
-        a = self.o.claim_an_id()
-        b = self.o.put_stream(a, "Some bytes to store", "foo.txt")
-        self.assertEquals(b['_filename'], "foo.txt")
-        self.assertEquals(b['_numberofbytes'], 19)
+        a = self.o.claim_a_bucket()
+        label = "foo.txt"
+        b = self.o.put_stream(a, label, "Some bytes to store")
+        self.assertEquals(b['_label'], "foo.txt")
+        self.assertEquals(b['_content_length'], 19)
         self.assertEquals(b['_checksum'], 'md5:eee89bbbcf416f658c7bc18cd8f2b61d')
     
     def test_store_bytes_with_params(self):
-        a = self.o.claim_an_id()
-        b = self.o.put_stream(a, "Some bytes to store", "foo.txt", {"a":"1", "b":[1,2,3,4,5]})
+        a = self.o.claim_a_bucket()
+        label = "foo.txt"
+        b = self.o.put_stream(a, label, "Some bytes to store", {"a":"1", "b":[1,2,3,4,5]})
         self.assertEquals(b['a'], "1")
         self.assertEquals(b['b'], [1,2,3,4,5])
+        self.assertEquals(b['_label'], "foo.txt")
+        self.assertEquals(b['_content_length'], 19)
+        self.assertEquals(b['_checksum'], 'md5:eee89bbbcf416f658c7bc18cd8f2b61d')
     
     def test_store_params_after_bytes(self):
-        a = self.o.claim_an_id()
-        self.o.put_stream(a, "Some bytes to store", "foo.txt")
-        b = self.o.update_stream_metadata(a, {"a":"1", "b":[1,2,3,4,5]})
+        a = self.o.claim_a_bucket()
+        label = "foo.txt"
+        self.o.put_stream(a, label, "Some bytes to store")
+        b = self.o.update_metadata(a, label, {"a":"1", "b":[1,2,3,4,5]})
         self.assertEquals(b['a'], "1")
         self.assertEquals(b['b'], [1,2,3,4,5])
         
     def test_params_persistence(self):
-        a = self.o.claim_an_id()
-        self.o.put_stream(a, "Some bytes to store", "foo.txt", {"a":"1", "b":[1,2,3,4,5]})
-        b = self.o.get_stream_metadata(a)        
+        a = self.o.claim_a_bucket()
+        label = "foo.txt"
+        self.o.put_stream(a, label, "Some bytes to store", {"a":"1", "b":[1,2,3,4,5]})
+        b = self.o.get_metadata(a, label)        
         self.assertEquals(b['a'], "1")
         self.assertEquals(b['b'], [1,2,3,4,5])
         
     def test_params_deletion(self):
-        a = self.o.claim_an_id()
-        self.o.put_stream(a, "Some bytes to store", "foo.txt", {"a":"1", "b":[1,2,3,4,5]})
-        self.o.remove_metadata_keys(a, ['b'])
-        b = self.o.get_stream_metadata(a)        
+        a = self.o.claim_a_bucket()
+        label = "foo.txt"
+        self.o.put_stream(a, label, "Some bytes to store", {"a":"1", "b":[1,2,3,4,5]})
+        self.o.remove_metadata_keys(a, label, ['b'])
+        b = self.o.get_metadata(a, label)     
         self.assertEquals(b['a'], "1")
-        self.assertFalse(b.has_key('b'))        
+        self.assertFalse(b.has_key('b'))
         
 if __name__ == '__main__':
     unittest.main()
