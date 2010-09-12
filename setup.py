d527b2428072857fd180d0c377e8dfc597f43607
rgrp <http://rufuspollock.org>
2010-09-12 19:44:34 +0100

    [setup][s]: add ofs.backend entry point and added all existing backends to that entry point.
      * also re-indent setup.py
      * add basic test for entry points

--- setup.py
+++ setup.py
@@ -2,14 +2,22 @@ from ez_setup import use_setuptools
 use_setuptools()
 from setuptools import setup, find_packages
 
-setup(name="OFS",
-      version="0.1",
-      description="OFS - provides plugin-orientated low-level blobstore.",
-      long_description="""OFS - provides plugin-orientated low-level blobstore. """,
-      maintainer="Ben O'Steen",
-      maintainer_email="bosteen@gmail.com",
-      license="http://www.apache.org/licenses/LICENSE-2.0",
-      packages=find_packages(),
-      test_suite = "test.test.TestPairtreeOFS",
-      )
+setup(
+    name="OFS",
+    version="0.1",
+    description="OFS - provides plugin-orientated low-level blobstore.",
+    long_description="""OFS - provides plugin-orientated low-level blobstore. """,
+    maintainer="Ben O'Steen",
+    maintainer_email="bosteen@gmail.com",
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    packages=find_packages(),
+    test_suite = "test.test.TestPairtreeOFS",
+    entry_points="""
+    [ofs.backend]
+    pairtree = ofs.local.pairtreestore:OFS
+    s3 = ofs.remote.botostore:S3OFS
+    google = ofs.remote.botostore:GSOFS
+    archive.org = ofs.remote.botostore:ArchiveOrgOFS
+    """
+    )
 
